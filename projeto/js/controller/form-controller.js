d40a29a8735037c68e8edd0601b56309ca3cd7ce
igor-lourenco <igor.lourencosantos@gmail.com>
2022-01-13 18:02:36 -0300

    Evento handleBtnClearClick

--- projeto/js/controller/form-controller.js
+++ projeto/js/controller/form-controller.js
@@ -10,7 +10,7 @@ function State(){
     this.inputCep = null;
     this.street = null;
     this.inputNumber = null;
-    this.inputCity = null;
+    this.inputCity =null;
     this.errorCep = null;
     this.errorNumber = null;
 }
@@ -27,9 +27,14 @@ export function init(){
     state.errorCep = document.querySelector('[data-error="cep"]');
     state.errorNumber = document.querySelector('[data-error="number"]');
 
+    //serve pra disparar um evento quando o usuario modificar o valor para vazio e clicar fora do input
     state.inputNumber.addEventListener('change', handleInputNumberChange);
+
+    //serve pra limpar o formulário quando o botão clear for clicado 
+    state.btnClear.addEventListener('click', handlerBtnClearClick);
 }
 
+//função para disparar um evento quando o valor do inputNumber foi modificado 
 function handleInputNumberChange(event){
     if(event.target.value == ""){
         setFormError("number", "Campo requerido")
@@ -38,8 +43,29 @@ function handleInputNumberChange(event){
     }
 }
 
+
+//função para disparar um evento quando o botao limpar for clicado
+function handlerBtnClearClick(event){
+    event.preventDefault();  //pro formulário não ser enviado e não chamar outra página
+    clearForm(); 
+
+}
+
 // função para escrever a mensagem de campo requerido no formulario
 function setFormError(key, value){
     const element = document.querySelector(`[data-error="${key}"]`);
     element.innerHTML = value;
+}
+
+//funcao para limpar os campos do formulario
+function clearForm(){
+    state.inputCep.value = "";
+    state.inputStreet.value = "";
+    state.inputNumber.value = "";
+    state.inputCity.value = "";
+
+    setFormError("cep", "");
+    setFormError("number", "");
+
+    state.inputCep.focus(); // pro cursor focar no input do cep
 }
